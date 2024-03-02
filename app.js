e3a649df75281883c1ab810ac9f47028f09be579
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-03-02 16:48:29 +0600

    Added banner

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 var swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    loop: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-        speed: 800,
+  loop: true,
+  autoplay: {
+    delay: 1100,
+    disableOnInteraction: false,
+    speed: 800,
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+    speed: 800,
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
     },
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-        speed: 800,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
 });
